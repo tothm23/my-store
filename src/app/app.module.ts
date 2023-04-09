93064e50020011f08519c841442535f748abd59f
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-09 13:05:28 +0200

    Create the product list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,9 +17,3 @@ import { ProductListComponent } from './product-list/product-list.component';
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
